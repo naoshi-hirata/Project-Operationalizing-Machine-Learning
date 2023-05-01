924e4da68597b887b1c3dbe5cb178daffd55fc34
naoshi-hirata <105419001+naoshi-hirata@users.noreply.github.com>
2023-05-01 08:11:07 +0000

    Add files via upload

--- starter_files/endpoint.py
+++ starter_files/endpoint.py
@@ -3,58 +3,60 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ''
+scoring_uri = 'http://a6921602-6e78-49d0-8eaa-0fb3300b1e7f.southcentralus.azurecontainer.io/score'
 # If the service is authenticated, set the key or token
-key = ''
+key = 'JHpEgBlTEclLjtyft25xVP6CahLijKt0'
 
 # Two sets of data to score, so we get two results back
-data = {"data":
-        [
-          {
-            "age": 17,
-            "campaign": 1,
-            "cons.conf.idx": -46.2,
-            "cons.price.idx": 92.893,
-            "contact": "cellular",
-            "day_of_week": "mon",
-            "default": "no",
-            "duration": 971,
-            "education": "university.degree",
-            "emp.var.rate": -1.8,
-            "euribor3m": 1.299,
-            "housing": "yes",
-            "job": "blue-collar",
-            "loan": "yes",
-            "marital": "married",
-            "month": "may",
-            "nr.employed": 5099.1,
-            "pdays": 999,
-            "poutcome": "failure",
-            "previous": 1
-          },
-          {
-            "age": 87,
-            "campaign": 1,
-            "cons.conf.idx": -46.2,
-            "cons.price.idx": 92.893,
-            "contact": "cellular",
-            "day_of_week": "mon",
-            "default": "no",
-            "duration": 471,
-            "education": "university.degree",
-            "emp.var.rate": -1.8,
-            "euribor3m": 1.299,
-            "housing": "yes",
-            "job": "blue-collar",
-            "loan": "yes",
-            "marital": "married",
-            "month": "may",
-            "nr.employed": 5099.1,
-            "pdays": 999,
-            "poutcome": "failure",
-            "previous": 1
-          },
-      ]
+data = {
+    "Inputs":{
+        "data":[
+            {
+                "age": 17,
+                "campaign": 1,
+                "cons.conf.idx": -46.2,
+                "cons.price.idx": 92.893,
+                "contact": "cellular",
+                "day_of_week": "mon",
+                "default": "no",
+                "duration": 971,
+                "education": "university.degree",
+                "emp.var.rate": -1.8,
+                "euribor3m": 1.299,
+                "housing": "yes",
+                "job": "blue-collar",
+                "loan": "yes",
+                "marital": "married",
+                "month": "may",
+                "nr.employed": 5099.1,
+                "pdays": 999,
+                "poutcome": "failure",
+                "previous": 1
+              },
+              {
+                "age": 87,
+                "campaign": 1,
+                "cons.conf.idx": -46.2,
+                "cons.price.idx": 92.893,
+                "contact": "cellular",
+                "day_of_week": "mon",
+                "default": "no",
+                "duration": 471,
+                "education": "university.degree",
+                "emp.var.rate": -1.8,
+                "euribor3m": 1.299,
+                "housing": "yes",
+                "job": "blue-collar",
+                "loan": "yes",
+                "marital": "married",
+                "month": "may",
+                "nr.employed": 5099.1,
+                "pdays": 999,
+                "poutcome": "failure",
+                "previous": 1
+              },
+          ]
+        }
     }
 # Convert to JSON string
 input_data = json.dumps(data)
